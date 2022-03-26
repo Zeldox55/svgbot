6d1bdd3018a24cc8aa06a8ac03af032c4054b597
Zeldox55 <39837384+Zeldox55@users.noreply.github.com>
2022-03-26 02:43:46 +0000

    Update config.js

--- configs/config.js
+++ configs/config.js
@@ -2,7 +2,7 @@ module.exports = {
     token: process.env.TOKEN,
     prefix: "+",
     version: "1.0.1",
-    mongo_uri: "mongodb+srv://zeldox:zeldoxlay@svgbot.6dkux.mongodb.net/svgbot",
+    mongo_uri: process.env.MONGO,
     owner: ["445167016765292545", "571013902230355998", "709089739218616351"]
   };
   
